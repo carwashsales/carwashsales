871c5a2347ab850d7adff30d97cd182df8179ad6
carwashsales <kasanjeclick@gmail.com>
2025-08-16 02:57:34 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
+  basePath: '/QuickCarSales',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -18,23 +15,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // devIndicators: {
-  //   allowedDevOrigins: [
-  //     '*.cloudworkstations.dev',
-  //   ],
-  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      '*.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
-
-
-// filepath: c:\Users\Sulaiman\Downloads\download (1)\next.config.js
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-});
-
-module.exports = withPWA({
-  // ...other Next.js config
-});
